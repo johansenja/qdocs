01fd4374c0b6529121bfc9eaaad90d5016e94f93
Joseph Johansen <joe@stotles.com>
2021-05-21 10:50:28 +0100

    Add specific support for active record

--- lib/qdocs.rb
+++ lib/qdocs.rb
@@ -41,102 +41,126 @@ module Qdocs
     rescue NameError
       raise UnknownClassError, "Unknown constant #{const}"
     end
-  end
 
-  class Const
-    include Helpers
-
-    def show(const)
-      const = const.to_s
-      constant = find_constant const
-
-      const_sl = Object.const_source_location const
-
+    def render_response(const, type, attrs)
       {
-        source_location: source_location_to_str(const_sl),
-        instance_methods: own_methods(constant.instance_methods).sort,
-        singleton_methods: own_methods(constant.methods).sort,
+        constant: {
+          name: const.name,
+          type: const.class.name
+        },
+        query_type: type,
+        attributes: attrs
       }
     end
   end
 
-  class Method
-    include Helpers
+  module Base
+    class Const
+      include Helpers
 
-    def index(const, pattern)
-      constant = find_constant const
-      {
-        constant: constant,
-        singleton_methods: own_methods(constant.methods.grep(pattern)).sort,
-        instance_methods: own_methods(constant.instance_methods.grep(pattern)).sort,
-      }
+      def show(const)
+        const = const.to_s
+        constant = find_constant const
+        yield constant if block_given?
+
+        const_sl = Object.const_source_location const
+
+        render_response(constant, :constant, {
+          source_location: source_location_to_str(const_sl),
+          instance_methods: own_methods(constant.instance_methods).sort,
+          singleton_methods: own_methods(constant.methods).sort,
+        })
+      end
     end
 
-    def show(const, meth, type)
-      constant = begin
-                   find_constant(const)
-                 rescue UnknownClassError
-                   abort "Unknown class #{const.inspect}"
+    class Method
+      include Helpers
+
+      def index(const, pattern)
+        constant = find_constant const
+
+        yield constant if block_given?
+
+        render_response(constant, :methods, {
+          constant: constant,
+          singleton_methods: own_methods(constant.methods.grep(pattern)).sort,
+          instance_methods: own_methods(constant.instance_methods.grep(pattern)).sort,
+        })
+      end
+
+      def show(const, meth, type)
+        constant = begin
+                     find_constant(const)
+                   rescue UnknownClassError
+                     abort "Unknown class #{const.inspect}"
+                   end
+        method = case meth
+                 when Symbol, String
+                   method_method = case type
+                                   when :instance
+                                     :instance_method
+                                   when :singleton, :class
+                                     :method
+                                   else
+                                     raise UnknownMethodTypeError, "Unknown method type #{type}"
+                                   end
+
+                   begin
+                     constant.send method_method, meth
+                   rescue NameError
+                     raise UnknownMethodError, "No method #{meth.inspect} for #{constant.inspect}. Did you mean #{constant.inspect}/#{meth}/ ?"
+                   end
+                 when Method
+                   meth
+                 else
+                   raise InvalidArgumentError, "#{meth.inspect} must be of type Symbol, String, or Method"
                  end
-      method = case meth
-               when Symbol, String
-                 method_method = case type
-                                 when :instance
-                                   :instance_method
-                                 when :singleton, :class
-                                   :method
-                                 else
-                                   raise UnknownMethodTypeError, "Unknown method type #{type}"
-                                 end
 
-                 begin
-                   constant.send method_method, meth
-                 rescue NameError
-                   raise UnknownMethodError, "No method #{meth.inspect} for #{constant.inspect}. Did you mean #{constant.inspect}/#{meth}/ ?"
+        parameters = params_to_hash(method.parameters)
+        src = method.source rescue nil
+        source = if src
+                   lines = src.lines
+                   first_line = lines.first
+                   indent_amount = first_line.length - first_line.sub(/^\s*/, '').length
+                   lines.map { |l| l[indent_amount..-1] }.join
                  end
-               when Method
-                 meth
-               else
-                 raise InvalidArgumentError, "#{meth.inspect} must be of type Symbol, String, or Method"
-               end
 
-      parameters = params_to_hash(method.parameters)
-      src = method.source rescue nil
-      source = if src
-                 lines = src.lines
-                 first_line = lines.first
-                 indent_amount = first_line.length - first_line.sub(/^\s*/, '').length
-                 lines.map { |l| l[indent_amount..-1] }.join
-               end
-
-      {
-        defined_at: source_location_to_str(method.source_location),
-        source: source,
-        arity: method.arity,
-        parameters: parameters,
-        comment: (method.comment.strip rescue nil),
-        name: method.name,
-        belongs_to: method.owner,
-        super_method: method.super_method,
-      }
+        render_response(constant, method_method, {
+          defined_at: source_location_to_str(method.source_location),
+          source: source,
+          arity: method.arity,
+          parameters: parameters,
+          comment: (method.comment.strip rescue nil),
+          name: method.name,
+          belongs_to: method.owner,
+          super_method: method.super_method,
+        })
+      end
     end
   end
 
   METHOD_REGEXP = /(?:[a-zA-Z_]+|\[\])[?!=]?/.freeze
   CONST_REGEXP = /[[:upper:]]\w*(?:::[[:upper:]]\w*)*/.freeze
 
+  Handler = if Object.const_defined? :ActiveRecord
+              require "qdocs/active_record"
+              Qdocs::ActiveRecord
+            else
+              Qdocs::Base
+            end
+
   def self.lookup(input)
     case input
     when /\A([[:lower:]](?:#{METHOD_REGEXP})?)\z/
-      Qdocs::Method.new.show(Object, $1, :instance)
+      Handler::Method.new.show(Object, $1, :instance)
     when /\A(#{CONST_REGEXP})\.(#{METHOD_REGEXP})\z/
-      Qdocs::Method.new.show($1, $2, :singleton)
+      Handler::Method.new.show($1, $2, :singleton)
     when /\A(#{CONST_REGEXP})#(#{METHOD_REGEXP})\z/
-      Qdocs::Method.new.show($1, $2, :instance)
+      Handler::Method.new.show($1, $2, :instance)
     when /\A(#{CONST_REGEXP})\z/
-      Qdocs::Const.new.show($1)
+      Handler::Const.new.show($1)
     when %r{\A(#{CONST_REGEXP})/([^/]+)/\z}
-      Qdocs::Method.new.index($1, Regexp.new($2))
+      Handler::Method.new.index($1, Regexp.new($2))
     else
       raise UnknownPatternError, "Unrecognised pattern #{input}"
     end
---
+++ lib/qdocs/active_record.rb
@@ -0,0 +1,94 @@
+module Qdocs
+  module ActiveRecord
+    class Const < Qdocs::Base::Const
+      include Helpers
+
+      def show(const)
+        database_attributes = {}
+        constant = nil
+        resp = super do |con|
+          if_active_record(con) do |klass|
+            constant = klass
+            klass.attribute_names.each do |name|
+              database_attributes[name] = active_record_column_for klass, name
+            end
+          end
+        end
+
+        if constant
+          {
+            **resp,
+            type: :active_record_class,
+            attributes: {
+              **resp[:attributes],
+              database_attributes: database_attributes
+            }
+          }
+        else
+          resp
+        end
+      end
+    end
+
+    class Method < Qdocs::Base::Method
+      include Helpers
+
+      def index(pattern)
+        database_attributes = {}
+        attrs = super do |constant|
+          if_active_record(constant) do |klass|
+            klass.attribute_names.grep(pattern).each do |name|
+              database_attributes[name] = active_record_column_for klass, name
+            end
+          end
+        end
+
+        if database_attributes.empty?
+          attrs
+        else
+          { **attrs, database_attributes: database_attributes }
+        end
+      end
+
+      def show(const, meth, type)
+        constant = nil
+        super do |klass|
+          constant = klass
+        end
+      rescue NameError => e
+        if constant && meth && type == :instance
+          if_active_record(constant) do |klass|
+            m = meth.is_a?(Method) ? (meth.name rescue nil) : meth
+            render_response(klass, :active_record_attribute, active_record_column_for(klass, m))
+          end
+        else
+          raise e
+        end
+      end
+    end
+
+    module Helpers
+      def active_record_columns_for(constant, name)
+        name = name.to_sym
+        col_attrs = constant.column_for_attribute(name)
+        if col_attrs.is_a? ActiveRecord::ConnectionAdapters::NullColumn
+          raise UnknownMethodError, "Unknown attribute #{name} for #{constant}"
+        end
+
+        {
+          type: col_attrs.sql_type_metadata.type,
+          comment: col_attrs.comment,
+          default: col_attrs.default,
+          null: col_attrs.null,
+          default_function: col_attrs.default_function,
+        }
+      end
+
+      def if_active_record(constant)
+        if Object.const_defined?("ActiveRecord::Base") && constant < ActiveRecord::Base
+          yield constant
+        end
+      end
+    end
+  end
+end
